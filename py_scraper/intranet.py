eec697a9558072c01323690cda7be55fd860cdeb
ems541a <ems541a@gmail.com>
2017-05-13 12:15:38 -0700

    Creating a new branch with refactor stuff

--- py_scraper/intranet.py
+++ py_scraper/intranet.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import getpass
 import http
 import os
+import re
 import requests
-
+import sys
 
 def log_in(session):
     """
@@ -43,3 +44,90 @@ def log_in(session):
         print("Failed to log in. Check your login information and try again.")
         return False
     return True
+
+def sort_tasks(soup):
+    tasks = soup.find_all('div', class_=" clearfix gap")
+    task_list = [Task(task) for task in tasks]
+
+def parse_li_tags(task):
+    for tag in task.find_all('li'):
+        if "GitHub" in tag.contents[0]:
+            repo = tag.contents[1].contents[0]
+        if "File" in tag.contents[0]:
+            filename = tag.contents[1].contents[0]
+        if "Directory" in tag.contents[0]:
+            directory = tag.contents[1].contents[0]
+    try:
+        return(repo, directory, filename)
+    except:
+        print("Errors parsing li tags to get GitHub repo, filename, directory.")
+        print("Exiting out!")
+        exit()
+
+def parse_task_number(task):
+    task_class = task.find('h4', class_="task")
+    number = task_class.contents[0].strip().split(' ')[0]
+    if number[-1] == '.':
+        number = number[:-1]
+    name = " ".join(task_class.contents[0].strip().split(' ')[1:])
+    if "mandatory" in task_class.find('span').contents[0]:
+        mandatory = True
+    else:
+        mandatory = False
+    return(name, number, mandatory)
+
+class Task:
+    def __init__(self, task):
+        self.id = task['id'][5:] # takes ex: task-875 and gives you 875
+        self.name, self.number, self.mandatory = parse_task_number(task)
+        self.repo, self.directory, self.filename = parse_li_tags(task)
+        self.main = parse_main(task, self.number)
+        print(self)
+
+    def __repr__(self):
+        desc = ["Task ID: {}".format(self.id),
+                "Task number: {}".format(self.number),
+                "Name: {}".format(self.name),
+                "Is mandatory: {}".format(self.mandatory),
+                "Repo: {}".format(self.repo),
+                "Dir: {}".format(self.directory),
+                "Filename: {}".format(self.filename),
+                "Main file: \n{}".format(self.main)]
+        return("\n".join(desc))
+
+
+def parse_main(task, number):
+    try:
+        code = task.find('pre').find('code')
+    except AttributeError: # triggers if there's no code blocks, thus no main
+        return(None)
+    if "cat {}-main".format(number) in code.contents[0]:
+        string = code.contents[0]
+        user = re.search('(.)*@ubuntu:(.)*', string)
+        string = string[user.end(2) + 1:]
+        user = re.search('(.)*@ubuntu:(.)*', string)
+        string = string[:user.start(0)]
+        return(string)
+    return(None)
+
+
+
+
+def parse_project(session, proj_number):
+#    url = 'https://intranet.hbtn.io/projects/'
+#    url += sys.argv[1]
+#    page = session.get(url)
+#    soup = BeautifulSoup(page.content, "lxml")
+    with open('result.html', 'r') as fd:
+        page = fd.read()
+    soup = BeautifulSoup(page, "lxml")
+    task_list = sort_tasks(soup)
+    print("Project Description:")
+    print(soup.find('article', class_="gap formatted-content"))
+
+if __name__ == '__main__':
+    session = requests.Session()
+#    log_in(session)
+#    proj_number = sys.argv[1]
+    proj_number = 215
+    parse_project(session, proj_number)
